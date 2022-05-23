19f101526f82b8bf85f429071612793615c6b3bd
Nurçin <nrcinozer@gmail.com>
2022-05-23 10:44:14 +0300

    changed subtitle with date

--- App.js
+++ App.js
@@ -17,37 +17,37 @@ const list = [
     name: "Amy Farha",
     avatar_url:
       "https://gravatar.com/avatar/8a41c8482a1875f91bd972a6210432dc?s=400&d=robohash&r=x",
-    subtitle: "Vice President",
+    date: "02/05/2022",
   },
   {
     name: "Chris Jackson",
     avatar_url:
       "https://gravatar.com/avatar/aa4b04c14bb1f7f97153e2a1d5bc76a9?s=400&d=robohash&r=x",
-    subtitle: "Vice Chairman",
+    date: "01/05/2022",
   },
   {
     name: "Amy Farha",
     avatar_url:
       "https://gravatar.com/avatar/8a41c8482a1875f91bd972a6210432dc?s=400&d=robohash&r=x",
-    subtitle: "Vice President",
+    date: "28/04/2022",
   },
   {
     name: "Chris Jackson",
     avatar_url:
       "https://gravatar.com/avatar/aa4b04c14bb1f7f97153e2a1d5bc76a9?s=400&d=robohash&r=x",
-    subtitle: "Vice Chairman",
+    date: "23/04/2022",
   },
   {
     name: "Amy Farha",
     avatar_url:
       "https://gravatar.com/avatar/8a41c8482a1875f91bd972a6210432dc?s=400&d=robohash&r=x",
-    subtitle: "Vice President",
+    date: "20/04/2022",
   },
   {
     name: "Chris Jackson",
     avatar_url:
       "https://gravatar.com/avatar/aa4b04c14bb1f7f97153e2a1d5bc76a9?s=400&d=robohash&r=x",
-    subtitle: "Vice Chairman",
+    date: "15/04/2022",
   },
 ];
 
@@ -226,7 +226,7 @@ function AccessLogScreen() {
             <Avatar source={{ uri: l.avatar_url }} />
             <ListItem.Content>
               <ListItem.Title>{l.name}</ListItem.Title>
-              <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+              <ListItem.Subtitle>{l.date}</ListItem.Subtitle>
             </ListItem.Content>
           </ListItem>
         ))}
